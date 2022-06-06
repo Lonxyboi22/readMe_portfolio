fa8b2611a4bb5d45c5867906bc426c67153e70a4
Lonxyboi <drewthemachine22@gmail.com>
2022-06-05 20:04:32 -0400

    added questions!

--- Develop/index.js
+++ Develop/index.js
@@ -1,25 +1,75 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 
-
-
 // TODO: Create an array of questions for user input
-const questions = [
-    inquirer
-        .prompt([
-            /* Pass your questions in here */
-          ])
-        .then((answers) => {
-            // Use user feedback for... whatever!!
-          })
-        .catch((error) => {
-            if (error.isTtyError) {
-              // Prompt couldn't be rendered in the current environment
-            } else {
-              // Something else went wrong
-            }
-          })
-];
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub Username'
+    },
+    {
+      type: 'input',
+      name: 'about',
+      message: 'Provide some information about yourself:'
+    }
+  ]);
+};
+
+const promptProject = () => {
+  console.log(`
+=================
+Add a New Project
+=================
+`);
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is the name of your project?'
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Provide a description of the project (Required)'
+    },
+    {
+      type: 'checkbox',
+      name: 'languages',
+      message: 'What did you build this project with? (Check all that apply)',
+      choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+    },
+    {
+      type: 'input',
+      name: 'link',
+      message: 'Enter the GitHub link to your project. (Required)'
+    },
+    {
+      type: 'confirm',
+      name: 'feature',
+      message: 'Would you like to feature this project?',
+      default: false
+    },
+    {
+      type: 'confirm',
+      name: 'confirmAddProject',
+      message: 'Would you like to enter another project?',
+      default: false
+    }
+  ]);
+};
+
+promptUser()
+  .then(answers => console.log(answers))
+  .then(promptProject)
+  .then(projectAnswers => console.log(projectAnswers));
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
